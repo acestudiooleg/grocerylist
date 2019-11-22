96f1b923df8a7df2f5c55675d7e8ffa3cd8efef0
Oleg Sudavniy <acestudiooleg@gmail.com>
2019-11-22 19:57:46 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { MaterialModule } from "./material/material.module";
+
+@NgModule({
+  declarations: [AppComponent, LoginPageComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
